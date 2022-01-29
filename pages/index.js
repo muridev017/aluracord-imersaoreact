31ea3cf4a8bfca98e5ddcc09ff08e86b6943812d
Murilo <murilomininel1@hotmail.com>
2022-01-29 00:41:08 -0300

    colocando state

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import {Box, Button, Text, TextField, Image} from '@skynexui/components';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React, { useEffect, useState } from 'react';
+import {useRouter} from 'next/router';
 import appConfig from '../config.json'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 
 
@@ -65,11 +40,18 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'muridev017';
+  // const username = '';
+  const [username, setUsername] = React.useState('');
+  const roteamento = useRouter();
+  const [isFormEnable, setIsFormEnable] = useState(false);
+
+  useEffect(() =>{
+    setIsFormEnable(username.length > 2);
+  }, [username]);
+
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -96,6 +78,13 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(infosDoEvento) {
+              infosDoEvento.preventDefault();
+              if (isFormEnable){
+              console.log('Alguém submeteu o form');
+              roteamento.push('/chat');
+            }
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -106,7 +95,29 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+            {/* <input
+              type='text'
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value)
+                //Onde ta o valor?
+                const valor = event.target.value;
+                //Trocar o valor da variavel
+                //Através do React e avise quem precisa
+                setUsername(valor);
+              }}
+            /> */}
+
             <TextField
+             value={username}
+             onChange={function (event) {
+              console.log('usuario digitou', event.target.value)
+              //Onde ta o valor?
+              const valor = event.target.value;
+              //Trocar o valor da variavel
+              //Através do React e avise quem precisa
+              setUsername(valor);
+            }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -119,9 +130,11 @@ export default function PaginaInicial() {
             />
             <Button
               type='submit'
-              label='Entrar'
+              label={!isFormEnable ? "Entrar" : 'Entrar'}
+              disabled={!isFormEnable}
               fullWidth
               buttonColors={{
+
                 contrastColor: appConfig.theme.colors.neutrals["000"],
                 mainColor: appConfig.theme.colors.primary[500],
                 mainColorLight: appConfig.theme.colors.primary[400],
